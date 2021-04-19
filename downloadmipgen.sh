30f3d6bc7689d6d277bc400d6208ccc8ebc1ea73
PriyaLakra <66781257+PriyaLakr@users.noreply.github.com>
2021-04-19 16:48:13 +0530

    Add files via upload

---
+++ downloadmipgen.sh
@@ -0,0 +1,23 @@
+#!/usr/bash
+
+echo "This is script for successfuly installing MIPgen and generating BWA index"
+
+# path_to_store_mipgen"
+inpath=$1
+
+cd $inpath
+
+git clone https://github.com/shendurelab/MIPGEN
+cd MIPGEN
+make
+
+echo "MIPGEN is successfully download in $input folder"
+
+
+tar -xf mipgen_example.tgz
+
+cd mipgen_practice
+
+
+
+
