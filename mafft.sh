a2725c458c3390aa18b868fecfbd1af8d19d2f45
PriyaLakra <66781257+PriyaLakr@users.noreply.github.com>
2021-04-19 14:41:13 +0530

    Add files via upload
    Give name of your fasta file as first argument, path of ouput folder as second argument, and path of input files as third argument. 
    
    Run -> bash mafft.sh filename outpath inpath

---
+++ mafft.sh
@@ -0,0 +1,17 @@
+#! /usr/bash
+
+echo "Give name of your fasta file as first argument, path of ouput folder as second argument, and path of input files as third argument. Run -> bash mafft.sh filename outpath inpath" 
+
+name=$(basename $1 .fasta) 
+in_data=$2
+out="$2/${name}_out"
+
+mkdir $in_data 
+cd $in_data 
+cp $3/$1 $in_data
+mkdir $out
+
+
+
+for i in $1; do mafft  --auto --leavegappyregion $i > $out/${name}_aligned.fasta; done
+
