6733e61230116cfacdd2188b7c6d2121ddcdfb87
PriyaLakra <66781257+PriyaLakr@users.noreply.github.com>
2021-06-17 00:43:17 +0530

    Update extract_fasta.sh

--- extract_fasta.sh
+++ extract_fasta.sh
@@ -3,6 +3,10 @@
 echo "Give the name of your gene of interest as first argument"
 echo "Give the name of the fasta file containing sequences as second argument"
 
+"""Extract fasta sequences with given IDs/names
+^-- get rid of line separator
+"""
+
 grep -A1 -i "$1" $2 | grep -v "^--" > $1.fasta 
 
 
