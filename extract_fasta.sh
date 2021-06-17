4d8656288c65f49a768226798ecd0fe4d98a6e93
PriyaLakra <66781257+PriyaLakr@users.noreply.github.com>
2021-06-17 11:20:51 +0530

    Update extract_fasta.sh

--- extract_fasta.sh
+++ extract_fasta.sh
@@ -11,5 +11,5 @@ grep -A1 -i "$1" $2 | grep -v "^--" > $1.fasta
 
 
 
-## --no-group-separator can be used instead of -v but  --no-group-separator is not working in mac grep
+## --no-group-separator should be used instead of -v but  --no-group-separator is not working in mac grep
 ## grep -w -A1 --no-group-separator -i "$1" $2  > $1.fasta 
